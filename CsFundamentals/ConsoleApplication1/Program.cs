a0f0011296519944c946b18303ff78181be7950a
Winbringer <voin1213@yandex.ru>
2016-06-11 22:54:24 +0700

    l1

---
+++ CsFundamentals/ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using System.Xml.Serialization;
+
+namespace ConsoleApplication1
+{
+
+    public class MyClass
+    {
+        [XmlAttribute]
+        public string Name { get; set; }
+        [XmlElement]
+        public int Age { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var s = File.Create("MyClass.xml"))
+            {
+                var x = new XmlSerializer(typeof(MyClass));
+                x.Serialize(s, new MyClass { Name = "Gordon", Age = 33 });
+            }
+            using (var s = File.Open("MyClass.xml", FileMode.Open))
+            {
+                var x = new XmlSerializer(typeof(MyClass));
+                var m = x.Deserialize(s) as MyClass;
+                Console.WriteLine($"Name {m.Name} Age {m.Age}");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ CsFundamentals/CsFundamentals/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Soap;
+
+namespace CsFundamentals
+{
+    [Serializable]
+    public class MyClass
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = File.Create("MyClass.xml");
+            var f = new SoapFormatter();
+            var m = new MyClass { Name = "G", Age = 1 };
+
+
+            f.Serialize(s, m);
+            s.Close();
+            var ss = File.Open("MyClass.xml", FileMode.Open);
+
+            MyClass m1 = f.Deserialize(ss) as MyClass;
+            ss.Dispose();
+            Console.WriteLine($"Name {m1.Name} Age {m1.Age}");
+            Console.ReadLine();
+
+        }
+    }
+}
